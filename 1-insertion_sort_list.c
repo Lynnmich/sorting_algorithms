a834a6b3880d62448b342776f0b53c091069a1a7
Lynnmich <lynnenmichuki@gmail.com>
2023-02-21 06:57:17 -0800

    Insertion_sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,24 +1,37 @@
 #include "sort.h"
 /**
- *swap: swaps two elements in the list
- *@list: doubly linked list to be sorted
- */
-void insertion_sort_list(listint_t **list);
-listint_t *swap(listint_t *head) {
-	listint_t * start = new listint_t();
-	start->next = head;
-	listint_t *curr = head, *prev = start
-		while(curr){
-		if(curr->next && (curr->next->val < curr->val)){
-			while(prev->next && (prev->next->val < curr->val))
-				prev = prev->next;
-			listint_t *temp = prev->next;
-			prev->next = curr->next;
-			curr->next = current->next->next;
-			prev->next->next =temp;
-			prev =start;
-		}else
-			curr = curr-> next;
+ * insertion_sort_list - sort a doubly linked list
+ * @list: Pointer to head of the list
+ * start: element to the left
+ * new: element to the right
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *new, *start;
+
+	if (!list || !(*list) || !(*list)->next)
+		return;
+	start = *list;
+	new = *list;
+	start = start->next;
+	while (start != NULL)
+	{
+		new = start;
+		start = start->next;
+		while (new->prev != NULL && new->n < new->prev->n)
+		{
+			if (new->next != NULL)
+				new->next->prev = new->prev;
+			if (new->prev->prev != NULL)
+				new->prev->prev->next = new;
+			else
+				*list = new;
+			new->prev->next = new->next;
+			new->next = new->prev;
+			temp = new->prev->prev;
+			new->prev->prev = new;
+			new->prev = temp;
+			print_list(*list);
 		}
-	return start->next
-		print_list(*list);
+	}
+}
